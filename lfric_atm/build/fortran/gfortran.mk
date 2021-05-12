8505f4ab8aeb5d6cec6b7d802bba547ed63ca1ac
christophermaynard <enquiries@metoffice.gov.uk>
2021-05-12 14:36:33 +0000

    ticket:2563 for Sam Cusworth. Apply more compile-time checks to socrates.
    git-svn-id: https://metomi/svn/lfric.xm/LFRic/trunk@28938 4bbb03d4-c1cc-11e1-9ca4-71d347b01581

--- lfric_atm/build/fortran/gfortran.mk
+++ lfric_atm/build/fortran/gfortran.mk
@@ -8,12 +8,18 @@
 
 $(info Project specials for GNU compiler)
 
-export FFLAGS_UM_PHYSICS = -fdefault-real-8
-# lfric_atm dependencies contain code with implicit lossy conversions and
+export FFLAGS_UM_PHYSICS = -fdefault-real-8 -fdefault-double-8
+# Most lfric_atm dependencies contain code with implicit lossy conversions and
 # unused variables.
 # We reset the FFLAGS_WARNINGS variable here in order to prevent
 # -Werror induced build failures.
 FFLAGS_WARNINGS          = -Wall -Werror=character-truncation -Werror=unused-value
+
+# But, we can apply full lfric infrastructure checking to socrates
+FFLAGS_SOCRATES_WARNINGS = -Werror=conversion -Werror=unused-variable
+
+science/src/socrates/%.o science/src/socrates/%.mod: export FFLAGS += $(FFLAGS_SOCRATES_WARNINGS)
+
 # We remove bounds checking (applied by -fcheck=all) and underflow checking. The 
 # latter is due to regular permitting of exponents going to zero for small numbers
 # to imply total extinction of radiation passing through a medium
